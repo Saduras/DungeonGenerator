b9c52c500e1f6b777cca712fe437b184e1514373
David Speck <david@saduras.de>
2015-11-08 15:08:42 +0100

    Add isMainRoom property to Room

--- Assets/SourceCode/Room.cs
+++ Assets/SourceCode/Room.cs
@@ -1,32 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class Room : MonoBehaviour 
 {
+	public Material defaultMaterial;
+	public Material mainRoomMaterial;
+
 	public float width { get { return transform.localScale.x; } }
 	public float length { get { return transform.localScale.z; } }
 
-	public float top 
-	{  
-		get { 
-			return transform.localPosition.z + length / 2;
-		}
-	}
-	public float bottom 
-	{  
-		get { 
-			return transform.localPosition.z - length / 2;
-		}
-	}
-	public float left
-	{  
-		get { 
-			return transform.localPosition.x - width / 2;
-		}
-	}
-	public float right
-	{  
-		get { 
-			return transform.localPosition.x + width / 2;
+	public float top  { get { return transform.localPosition.z + length / 2; } }
+	public float bottom { get { return transform.localPosition.z - length / 2; } }
+	public float left { get { return transform.localPosition.x - width / 2; } }
+	public float right { get { return transform.localPosition.x + width / 2; } }
+
+	public bool isMainRoom
+	{
+		set {
+			GetComponent<MeshRenderer>().material = value ? mainRoomMaterial : defaultMaterial;
 		}
 	}
 
