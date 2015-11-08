dd2215799baec539cefa7a7f426d6e1b5d7252f9
David Speck <david@saduras.de>
2015-11-08 16:45:28 +0100

    Add utility method for converting lists into strings

---
+++ Assets/SourceCode/Utility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public static class Utility
+{
+	public static string ListToString<T>(List<T> list)
+	{
+		var str = "";
+		list.ForEach(item => str += item.ToString() + ",");
+
+		return str;
+	}
+}
+
